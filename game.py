37154aaaf6ddcabc2f1bdce4c8ac1ca0d620783c
nsh240 <74507655+nsh240@users.noreply.github.com>
2021-05-25 20:42:56 -0400

    more python added

--- game.py
+++ game.py
@@ -4,3 +4,11 @@ print("Rock, Paper, Scissors, Shoot!")
 
 user_choice = input("Please choose one of'rock','paper','scissors':")
 print("USER CHOICE: ", user_choice)
+
+if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
+    print("Valid, keep going")
+else:
+    print("invalid input")
+exit()
+
+print("This is the end of our game, please play again.")
