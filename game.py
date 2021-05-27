b6c9371376886f245bae287c21e2a6efd03093ef
nsh240 <74507655+nsh240@users.noreply.github.com>
2021-05-27 19:00:46 -0400

    removed comments

--- game.py
+++ game.py
@@ -27,20 +27,6 @@ print("Computer choice: ", computer_choice)
 
 if user_choice==computer_choice:
     print("It's a Tie.")
-# elif user_choice=="rock" and computer_choice=="scissors":
-#     print("The User wins")
-# elif computer_choice=="rock" and user_choice=="scissors":
-#     print("The Computer wins")
-# elif user_choice=="paper" and computer_choice=="rock":
-#     print("The User wins")
-# elif computer_choice=="paper" and user_choice=="rock":
-#     print("The Computer wins")
-# elif user_choice=="scissors" and computer_choice=="paper":
-#     print("The User wins")
-# elif computer_choice=="scissors" and user_choice=="paper":
-#     print("The Computer wins")
-
-#refactored below
 
 if user_choice== "rock":
     if computer_choice=="paper":
